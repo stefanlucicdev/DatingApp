32ff2f1f4c0dccd3923a5b1b67173e335c191b7c
stefanlucicdev@gmail.com <stefanlucicdev@gmail.com>
2020-11-19 16:56:36 +0100

    End of Section 7

--- API/Startup.cs
+++ API/Startup.cs
@@ -19,6 +19,7 @@ using API.Extensions;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using API.Middleware;
 
 namespace API
 {
@@ -49,12 +50,17 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+
+            app.UseMiddleware<ExceptionMiddleware>();
+
+            /*
+             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
+            */
 
             app.UseHttpsRedirection();
 
