6bfc9e9ecd3f19c61fdd6d0672e3950d6285fcb7
stefanlucicdev <stefanlucicdev@gmail.com>
2021-06-08 22:06:21 +0200

    Section 19 pre switch to PostgreSql

--- API/Startup.cs
+++ API/Startup.cs
@@ -74,11 +74,16 @@ namespace API
 
             app.UseAuthorization();
 
+            app.UseDefaultFiles();
+
+            app.UseStaticFiles();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
                 endpoints.MapHub<PresenceHub>("hubs/presence");
                 endpoints.MapHub<MessageHub>("hubs/message");
+                endpoints.MapFallbackToController("Index", "Fallback");
             });
         }
     }
